b678d836acab99912a42f68ec198d9728d2c3f72
VinicinBraga <vinicius@bragamatta.com.br>
2022-11-22 14:25:22 -0300

    appointments in memory

--- src/user-cases/create-appointment.spec.ts
+++ src/user-cases/create-appointment.spec.ts
@@ -1,19 +1,63 @@
-import { describe,expect, it } from "vitest";
+import { describe, expect, it } from "vitest";
 import { Appointment } from "../entities/appointment";
 import { CreateAppointment } from './create-appointment';
 import { getFutureDate } from '../tests/utils/get-future-date';
+import { InMemoryAppointmentRepository } from '../repositories/in-memory/in-memory-appointment-repository';
 
 describe('Create Appointment', () => {
   it('should be able to create appointment', () => {
     
     const startAt = getFutureDate('2022-08-10')
     const endsAt = getFutureDate('2022-08-11')
-    const createAppointment = new CreateAppointment()
+
+    const appointmentRepository = new  InMemoryAppointmentRepository()
+    const createAppointment = new CreateAppointment(appointmentRepository)
 
     expect(createAppointment.execute({
       customer: 'John Doe',
       startAt,
       endsAt,
     })).resolves.toBeInstanceOf(Appointment)
+  });
+
+  it('should not be able to create appointment', async () => {
+    
+    const startAt = getFutureDate('2022-08-10')
+    const endsAt = getFutureDate('2022-08-15')
+
+    const appointmentRepository = new  InMemoryAppointmentRepository()
+    const createAppointment = new CreateAppointment(
+      appointmentRepository
+    )
+    
+    await createAppointment.execute({
+      customer: 'John Doe',
+      startAt,
+      endsAt,
+    })
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startAt: getFutureDate('2022-08-14'),
+      endsAt: getFutureDate('2022-08-18')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startAt: getFutureDate('2022-08-08'),
+      endsAt: getFutureDate('2022-08-12')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startAt: getFutureDate('2022-08-08'),
+      endsAt: getFutureDate('2022-08-17')
+    })).rejects.toBeInstanceOf(Error)
+
+    expect(createAppointment.execute({
+      customer: 'John Doe',
+      startAt: getFutureDate('2022-08-11'),
+      endsAt: getFutureDate('2022-08-12')
+    })).rejects.toBeInstanceOf(Error)
   });
 });
--- src/user-cases/create-appointment.ts
+++ src/user-cases/create-appointment.ts
@@ -1,4 +1,5 @@
 import { Appointment } from '../entities/appointment'
+import { AppointmentRepository } from '../repositories/appointment-repository';
 
 interface CreateAppointmentRequest {
   customer: String;
@@ -9,16 +10,31 @@ interface CreateAppointmentRequest {
 type CreateAppointmentResponse = Appointment
 
 export class CreateAppointment {
+  constructor(
+    private appointmentRepository: AppointmentRepository
+  ) { }
+  
   async execute ({
     customer,
     startAt,
     endsAt
   }: CreateAppointmentRequest): Promise<CreateAppointmentResponse> {
+    const overlappingAppointment = await this.appointmentRepository.findOverlappingAppointment(
+      startAt,
+      endsAt
+    )
+
+    if (overlappingAppointment) {
+      throw new Error ('Another appointment overlaps this appointment')
+    }
+
     const appointment = new Appointment({
       customer,
       startAt,
       endsAt
     })
+
+    await this.appointmentRepository.create(appointment)
     
     return appointment
   } 
